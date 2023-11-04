234233054fb0e3263dbb26102309c0811a7bb48b
Shifani16 <110917558+Shifani16@users.noreply.github.com>
2023-11-04 21:29:27 +0700

    yeah

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ let text = document.getElementById('text');
 window.addEventListener('scroll', function () {
     let value = window.scrollY;
 
-    header.style.top = value * 0.5 + 'px';
     btn.style.marginTop = value * 1.5 + 'px';
     text.style.marginTop = 50 + value * -0.5 + + '%';
 
 })
 
-window.addEventListener('scroll', function () {
-    const header = document.getElementById('header');
-    if (window.scrollY > 0) {
-        header.classList.add('sticky');
-    } else {
-        header.classList.remove('sticky');
-    }
-});
+function setActive(clickedElement) {
+    const listItems = document.querySelectorAll("#header ul li a");
+    listItems.forEach(items => {
+        items.classList.remove("active");
+    });
+
+    clickedElement.classList.add("active")
+}
+
+
